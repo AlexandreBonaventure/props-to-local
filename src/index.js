bc7cbe7c1c57e329a2c8e40dbd727017f86f9618
Alexandre Bonaventure <ageissmann@lichencommunications.com>
2017-03-24 14:05:47 -0400

    add es build (expose es module in package) + add custom formatter

--- src/index.js
+++ src/index.js
@@ -6,21 +6,27 @@ import {
 const defaultOptions = {
   localName: 'local',
 };
+const defaultFormatter = (value) => value
 
-export default (props, options = defaultOptions) => ({
-  props,
-  data() {
-    return {
-      [options.localName]: {
-        ...mapValues(props, (prop, propName) => this[propName]),
-      },
-    };
-  },
-  created() {
-    each(props, (prop, propName) => {
-      this.$watch(propName, (value) => {
-        this[options.localName][propName] = value;
+export default (props, options = defaultOptions) => {
+  const propsDefinitions = mapValues(props, ({ ...prop, formatter }) => ({
+    ...prop,
+  }))
+  return {
+    props: propsDefinitions,
+    data() {
+      return {
+        [options.localName]: {
+          ...mapValues(props, ({ formatter = defaultFormatter }, propName) => formatter(this[propName])),
+        },
+      };
+    },
+    created() {
+      each(props, ({ formatter = defaultFormatter }, propName) => {
+        this.$watch(propName, (value) => {
+          this[options.localName][propName] = formatter(value);
+        });
       });
-    });
-  },
-});
+    },
+  }
+};
